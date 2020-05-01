7974b97fbd76077075a4b4000c45df7de0b9a9f1
Hassan Alzlubair <cs.hassan@hotmail.com>
2020-05-01 09:16:50 +0200

    Fix account controller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,53 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Transaction;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
-    public function transfeer(Request $request){
-
+    public function transfeer(Request $request)
+    {
         $receiver = $request->to;
-        
         $amount = $request->amount;
-        
         $sender = Auth::user();
-        
-        
-        if($sender->balance < $amount){
-        
-        return response()->json(400,[
-        "Error"=>" the amount is bigger than your balance"]);
-        
+
+        if ($sender->balance < $amount) {
+            return response()->json([
+                "Error" => " the amount is bigger than your balance"],
+                400);
         }
-        
-        
-        
+
+
         $receiver = User::find($receiver);
         $receiver->balance += $amount;
         $receiver->save();
-        
-        
-        
+
         $sender->balance -= $amount;
         $sender->save();
-        
+
         $trans = Transaction::create([
-        "sender" => $sender->id,
-        "receiver" => $receiver->id,
-        "amount" => $amount
+            "sender" => $sender->id,
+            "receiver" => $receiver->id,
+            "amount" => $amount
         ]);
-        
-        
-        
-        return response()->json(200,[
-        
-        "transaction_id" => $trans->id,
-        "current_balance" => $sender->balance
-        
-        ]);
-        
-        }
+
+
+        return response()->json([
+            "transaction_id" => $trans->id,
+            "current_balance" => $sender->balance
+        ], 200);
 
     }
+
+}
 
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/transfeer','AccountController@transfeer');
+Route::post("/auth","AuthController@auth");
